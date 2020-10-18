d4e6a8acf7b0adef83bf29546890671e52ba1e85
Yuxiaoy1 <1032897296@qq.com>
2020-10-19 01:04:20 +0800

    add homepage

---
+++ .vuepress/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    title: 'Awesome Newsletters',
+    description: '🎉A curated list of awesome newsletters for indiehackers or people interested in side projects, product, design and growth marketing.',
+    base: "/awesome-newsletters/"
+}
