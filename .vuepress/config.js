e5b1697735a37ed06a2e8f28dbd4199d0e29b464
Yuxiaoy1 <1032897296@qq.com>
2020-10-19 20:39:47 +0800

    add docs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     title: 'Awesome Newsletters',
     description: '🎉A curated list of awesome newsletters for indiehackers or people interested in side projects, product, design and growth marketing.',
-    base: "/awesome-newsletters/"
+    base: "/awesome-newsletters/",
+    dest: "./docs/"
 }
