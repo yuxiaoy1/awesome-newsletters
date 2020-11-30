dc026e842529872b5a11d15c02565440ae4d1603
yuxiaoy <1032897296@qq.com>
2020-11-30 21:06:12 +0800

    add github icon and link

--- .vuepress/theme/Layout.vue
+++ .vuepress/theme/Layout.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="theme-light">
-        <button class="themeBtn" @click="toggleTheme()">🌝<span>/</span>🌚</button>
+        <header>
+            <button class="theme-btn" @click="toggleTheme()">🌝<span>/</span>🌚</button>
+            <a href="https://www.github.com/yuxiaoy1/awesome-newsletters/" target="_blank">
+                <svg class="octicon octicon-mark-github v-align-middle" height="30" viewBox="0 0 16 16" version="1.1"
+                    width="30" aria-hidden="true">
+                    <path fill-rule="evenodd"
+                        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z">
+                    </path>
+                </svg>
+            </a>
+        </header>
         <Content />
     </div>
 </template>
@@ -63,18 +73,21 @@ body {
     background-color: var(--bg-color);
     transition: ease 0.5s;
 }
-.themeBtn {
-    display: block;
-    margin-left: auto;
-    margin-right: 3rem;
-    padding: 2rem 1rem;
+header{
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding: 2rem 2rem;
+}
+.theme-btn {
+    padding: 0 2rem;
     background-color: transparent;
     font-size: 30px;
     cursor: pointer;
     border: none;
     outline: none;
 }
-.themeBtn span{
+.theme-btn span {
     color: var(--heading-color);
 }
 h1 {
